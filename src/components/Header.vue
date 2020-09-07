7bca46f96b1a4998ad7d8cdcbc1a4e93121b83fb
Abby <abbyberry19>
2020-09-07 09:25:56 +0100

    Web Templates

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,33 @@
 <template>
-    <h1>The Header Component</h1>
+    <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+          <router-link to="/portfolio" activeClass="active" tag="li"><a>Portfolio</a></router-link>
+          <router-link to="/stocks" activeClass="active" tag="li"><a>Stocks</a></router-link>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">End Day</a></li>
+        <li class="dropdown">
+          <a 
+            href="#" 
+            class="dropdown-toggle" 
+            data-toggle="dropdown" 
+            role="button" a
+            ria-haspopup="true" 
+            aria-expanded="false">Save & Load <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Save</a></li>
+            <li><a href="#">Load</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
 </template>
