6f4e44e9fff533c9892893e4a892a1acb5f8fa89
Abby <abbyberry19>
2020-09-09 10:22:45 +0100

    Selling Bug Fix

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -40,16 +40,16 @@
             }
         },
         methods: {
-                ...mapActions([
-                    'sellStock'
-                ]),
+                ...mapActions({
+                    placeSellOrder:'sellStock'
+                }),
             sellStock() {
                 const order = {
                     stockId: this.stock.id,
                     stockPrice: this.stock.price,
                     quantity: this.quantity
                 };
-                this.sellStock(order);
+                this.placeSellOrder(order);
                 this.quantity=0;
             }
         }
